f03499e4176b0a3e19b414e7e7f987fc8aad0226
= <=>
2013-08-13 16:13:03 -0700

    gui!

--- post-it.js
+++ post-it.js
@@ -1,61 +1,74 @@
 
-// var Board = function( selector ) {
-//   // Your board related code goes here
-  
-//   // Use $elem to access the DOM element for this board
-
-//   var $elem = $( selector );
-  
-//   function initialize() {
-//     this.PostItsGroup = []
-//     // What needs to happen when this object is created?
-//   };
-
-//   initialize();
-// };
-
-var Board = function() {
-  this.PostItsGroup = []
-}
-
- b 
-
-  deletePostIt: function(PostIt)
-
-  renderBoard: function() {
-    PostIt.renderPostIt();
-  },
-}
-
-var PostIt = function() {
-  // Your post-it related code goes here
-  this.content = ""
-  this.coord = $(this).position();
+var Board = function( selector ) {
+  this.postItsGroup = []
+  var $elem = $( selector );
 };
 
-PostIt.prototype = {
-  updateposition: function() {
-    this.coord = $(this).position
-  }
+Board.prototype = {
+  addpostIt: function(postIt) {
+    this.postItsGroup.push(postIt);
+  },
 
-  renderPostIt: function() {
+  deletepostIt: function(postIt) {
+    var i = this.postItsGroup.indexOf(postIt);
+    this.postItsGroup.splice(i,1)
+ }
+}
+
+var postIt = function() {
+  this.content = ""
+  // this.coord = $(this).position();
+};
+
+postIt.prototype = {
+  // updateposition: function(){
+  //   this.coord = $(this).position();
+  // };
+
+  renderpostIt: function(location) {
     this.domElement = $("<div class='post-it'></div>");
-    this.header = "<div class='header'><a href='#'> x </a></div>";
-    this.content = "<div class='content'>" + this.content + "</div>";
-    // this.domElement.append(header);
-    // this.domElement.append(content);
+    var header = "<div class='header'><a href='#'> x </a></div>";
+    var content = "<div class='content'>" + this.content + "</div>";
+    this.domElement.append(header);
+    this.domElement.append(content);
+    this.domElement.position({
+      of: event
+    });
+    $('#board').append(this.domElement);
+  },
+
+  renderContent: function(postIt) {
+    this.content = postIt.val()
 
   }
-
-
 }
 
 $(function() {
-  // Board.new('#board');
-  // post.renderPostIt();
 
-  $('.post-it').draggable();
-  // This code will run when the DOM has finished loading
+  board = new Board('#board');
+  
+   $('#board').on('click', '.content', function(event){
+    event.stopPropagation();
+    console.log('create content')
+    // console.log($(this).createElement('p'))
+    console.log(document.createElement('p'))
+  })  
+
+  $('#board').on('click', function(e){
+    var newone = new postIt()
+    board.addpostIt(newone);
+    newone.renderpostIt(e);
+    $('.post-it').draggable({ handle: ".header"})
+  })
+  
+ $('#board').on('click', 'a', function(event){
+    event.stopPropagation();
+    console.log('delete post')
+    board.deletepostIt($(this).closest('.post-it'))
+    $(this).closest('.post-it').remove();
+  });
+
+ 
   
 });
 
